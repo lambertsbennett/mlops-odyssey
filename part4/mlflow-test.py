881af65c5bc1c5abc71a08f9dd58442333001947
lambertsbennett <lambertsbennett@gmail.com>
2022-04-11 08:30:10 +0200

    add part 4 initial files

---
+++ part4/mlflow-test.py
@@ -0,0 +1,30 @@
+from random import random, randint
+from sklearn.ensemble import RandomForestRegressor
+import os
+
+import mlflow
+import mlflow.sklearn
+
+mlflow.set_tracking_uri('http://localhost:5000')
+
+mlflow.set_experiment("test-localhost-1")
+
+with mlflow.start_run(run_name="YOUR_RUN_NAME") as run:
+    params = {"n_estimators": 5, "random_state": 42}
+    sk_learn_rfr = RandomForestRegressor(**params)
+
+    # Log parameters and metrics using the MLflow APIs
+    mlflow.log_params(params)
+    mlflow.log_param("param_1", randint(0, 100))
+    mlflow.log_metrics({"metric_1": random(), "metric_2": random() + 1})
+
+    os.environ["AWS_ACCESS_KEY_ID"] = "admin"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "dYzac5sD8R"
+    os.environ['MLFLOW_S3_ENDPOINT_URL'] = 'http://localhost:9001'
+
+    # Log the sklearn model and register as version 1
+    mlflow.sklearn.log_model(
+        sk_model=sk_learn_rfr,
+        artifact_path="sklearn-model",
+        registered_model_name="sk-learn-random-forest-reg-model"
+    )
